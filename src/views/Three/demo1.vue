0bef35033ba56d2869900de23b148026c33ae713
yjliu <1229931433@qq.com>
2021-05-11 17:53:33 +0800

    方法分解

--- src/views/Three/demo1.vue
+++ src/views/Three/demo1.vue
@@ -16,39 +16,44 @@ export default {
   },
   created () {},
   mounted () {
-    this.init()
+    this.initThree()
+    this.initCamera()
+    this.initScene()
+    this.initGeometry()
+    this.initHelper()
     this.render()
   },
   watch: {},
   methods: {
-    init: function () {
+    // 渲染器设置
+    initThree () {
       const container = document.getElementById('demo')
-      /**
-       * 创建三要素
-       * 场景scene、相机camera、渲染器renderer
-       */
-      this.scene = new Three.Scene() // 定义场景
-
-      // 相机设置
-      this.camera = new Three.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.01, 10)
+      this.renderer = new Three.WebGLRenderer({ antialias: true })
+      this.renderer.setClearColor(0x000000)
+      this.renderer.setSize(container.clientWidth, container.clientHeight)
+      container.appendChild(this.renderer.domElement)
+    },
+    // 相机设置
+    initCamera () {
+      this.camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10)
       this.camera.position.z = 1
-
-      // 添加场景
+    },
+    // 场景设置
+    initScene () {
+      this.scene = new Three.Scene()
+    },
+    // 添加六面体 
+    initGeometry () {
       const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2) // 定义长宽高
       const material = new Three.MeshNormalMaterial()
 
       this.mesh = new Three.Mesh(geometry, material)
       this.scene.add(this.mesh)
-
-      // 添加网格线
+    },
+    // 添加网格线
+    initHelper () {
       const helper = new Three.GridHelper(1000, 50, 0x0000ff, 0x808080)
       this.scene.add(helper)
-
-      // 渲染器设置
-      this.renderer = new Three.WebGLRenderer({ antialias: true })
-      this.renderer.setClearColor(0x000000)
-      this.renderer.setSize(container.clientWidth, container.clientHeight)
-      container.appendChild(this.renderer.domElement)
     },
     render () {
       requestAnimationFrame(this.render)
